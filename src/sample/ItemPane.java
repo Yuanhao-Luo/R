00ff89927e8d8ab389ba45943214ddff0b584d27
fanYiKun <1375122453@qq.com>
2021-11-21 14:33:16 +0800

    小整合版，整合了黎汇枫版的物品系统，琨老师的maze part demo

--- src/sample/ItemPane.java
+++ src/sample/ItemPane.java
@@ -7,18 +7,44 @@ import javafx.scene.layout.Pane;
 public class ItemPane extends Pane {
     String url;
     ImageView imageView;
+    boolean select = false;
+    ImageView hover;
+    ImageView bet;
+
 
     public ItemPane(int num){
         super();
-        int[] itemx = {190,287,383,477,572,674,765};
+        int[] itemx = {177,275,373,470,564,659,755};
         int itemy = 595;
-        setLayoutX(itemx[num]);
+        setLayoutX(itemx[num%7]);
         setLayoutY(itemy);
+        setVisible(false);
 
         imageView = new ImageView();
-        imageView.setFitWidth(100);
-        imageView.setFitHeight(100);
+        imageView.setFitWidth(88);
+        imageView.setFitHeight(88);
         getChildren().add(imageView);
+
+        hover = new ImageView(new Image("file:.\\images\\Item_hover.png"));
+        hover.setVisible(false);
+        getChildren().add(hover);
+
+        bet = new ImageView(new Image("file:.\\images\\bet.png"));
+        bet.setVisible(false);
+        bet.setX(3.6);
+        bet.setY(3);
+        getChildren().add(bet);
+
+        setOnMouseEntered(e -> {
+            hover.setVisible(true);
+        });
+        setOnMouseExited(e -> {
+            hover.setVisible(false);
+        });
+        setOnMouseReleased(e -> {
+            setSelect(!isSelect());
+            System.out.println("adsfouhiaoeiuhfa");
+        });
     }
 
     public void setImage(String url){
@@ -26,4 +52,15 @@ public class ItemPane extends Pane {
         Image image = new Image("file:"+url);
         imageView.setImage(image);
     }
+
+    public boolean isSelect() {
+        return select;
+    }
+
+    public void setSelect(boolean select) {
+        this.select = select;
+        bet.setVisible(select);
+    }
+
+
 }
