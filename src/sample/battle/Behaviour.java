514687736b4f6b3b0372c9c6373fab54783b8df0
jpy8500 <2285992973@qq.com>
2021-11-30 14:10:10 +0800

    1409

---
+++ src/sample/battle/Behaviour.java
@@ -0,0 +1,13 @@
+package sample.battle;
+
+public class Behaviour {
+    String url;
+
+    public Behaviour(String url){
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
