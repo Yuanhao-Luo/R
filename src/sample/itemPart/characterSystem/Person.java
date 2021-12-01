1e9b7a3fccdf59c07407639d880f35b0acbeb5c8
Luo <1097308076@qq.com>
2021-12-01 08:43:59 +0800

    结算页面事件

--- src/sample/itemPart/characterSystem/Person.java
+++ src/sample/itemPart/characterSystem/Person.java
@@ -31,11 +31,11 @@ public class Person {
         simpleFactory s = new simpleFactory();
         itemList.add(s.buildMagicSword());
         itemList.add(s.buildMagicSword());
+        itemList.add(s.buildGuerrillaSword());
+        itemList.add(s.buildGuerrillaSword());
+        itemList.add(s.buildSmallShield());
         itemList.add(s.buildMagicSword());
-        itemList.add(s.buildMagicSword());
-        itemList.add(s.buildMagicSword());
-        itemList.add(s.buildMagicSword());
-        itemList.add(s.buildMagicSword());
+        itemList.add(s.buildTH180());
         itemList.add(s.buildBigStick());
     }
 
