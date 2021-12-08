ce0863252e4cb31fe685f8cb2d85b95a737ee36f
jpy8500 <2285992973@qq.com>
2021-12-08 10:46:43 +0800

    1046

--- src/sample/battle/MonsterFactory.java
+++ src/sample/battle/MonsterFactory.java
@@ -18,11 +18,13 @@ public class MonsterFactory {
     }
 
     public Monster buildHoney(){
-        String b2a_url = "";
-        Attack b2a = new Attack(b2a_url, 60);
+        String b1a_url = "";
+        Attack b1a = new Attack(b1a_url, 60);
+        Attack b2a = new Attack(b1a_url, 100);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
         BehaviorLogic b2 = new BehaviorLogic(b2a, null);
 
-        BehaviorLogic[] bls = {b2};
+        BehaviorLogic[] bls = {b1,b2};
 
         String monster_url = ".\\images\\monster\\honey.png";
         int maxHp = 200;
@@ -33,11 +35,11 @@ public class MonsterFactory {
     }
 
     public Monster buildFox(){
-        String b3a_url = "";
-        Attack b3a = new Attack(b3a_url, 100);
-        BehaviorLogic b3 = new BehaviorLogic(b3a, null);
+        String b1a_url = "";
+        Attack b1a = new Attack(b1a_url, 30);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
 
-        BehaviorLogic[] bls = {b3};
+        BehaviorLogic[] bls = {b1};
 
         String monster_url = ".\\images\\monster\\fox.png";
         int maxHp = 72;
@@ -48,11 +50,11 @@ public class MonsterFactory {
     }
 
     public Monster buildFrog(){
-        String b4a_url = "";
-        Attack b4a = new Attack(b4a_url, 40);
-        BehaviorLogic b4 = new BehaviorLogic(b4a, null);
+        String b1a_url = "";
+        Attack b1a = new Attack(b1a_url, 40);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
 
-        BehaviorLogic[] bls = {b4};
+        BehaviorLogic[] bls = {b1};
 
         String monster_url = ".\\images\\monster\\frog.png";
         int maxHp = 96;
@@ -63,11 +65,11 @@ public class MonsterFactory {
     }
 
     public Monster buildHorse(){
-        String b5a_url = "";
-        Attack b5a = new Attack(b5a_url, 700);
-        BehaviorLogic b5 = new BehaviorLogic(b5a, null);
+        String b1a_url = "";
+        Attack b1a = new Attack(b1a_url, 700);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
 
-        BehaviorLogic[] bls = {b5};
+        BehaviorLogic[] bls = {b1,b1};
 
         String monster_url = ".\\images\\monster\\horse.png";
         int maxHp = 1480;
@@ -78,11 +80,11 @@ public class MonsterFactory {
     }
 
     public Monster buildWorm(){
-        String b6a_url = "";
-        Attack b6a = new Attack(b6a_url, 50);
-        BehaviorLogic b6 = new BehaviorLogic(b6a, null);
+        String b1a_url = "";
+        Attack b1a = new Attack(b1a_url, 50);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
 
-        BehaviorLogic[] bls = {b6};
+        BehaviorLogic[] bls = {b1};
 
         String monster_url = ".\\images\\monster\\worm.png";
         int maxHp = 180;
@@ -93,11 +95,14 @@ public class MonsterFactory {
     }
 
     public Monster buildThief(){
-        String b7a_url = "";
-        Attack b7a = new Attack(b7a_url, 30);
-        BehaviorLogic b7 = new BehaviorLogic(b7a, null);
+        String b1a_url = "";
+        String b1d_url = "";
+        Attack b1a = new Attack(b1a_url, 30);
+        Defence b1d = new Defence(b1d_url, 30);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
+        BehaviorLogic b2 = new BehaviorLogic(null, b1d);
 
-        BehaviorLogic[] bls = {b7};
+        BehaviorLogic[] bls = {b2,b1,b1};
 
         String monster_url = ".\\images\\monster\\thief.png";
         int maxHp = 64;
@@ -108,11 +113,14 @@ public class MonsterFactory {
     }
 
     public Monster buildRedThief(){
-        String b8a_url = "";
-        Attack b8a = new Attack(b8a_url, 50);
-        BehaviorLogic b8 = new BehaviorLogic(b8a, null);
+        String b1a_url = "";
+        String b1d_url = "";
+        Attack b1a = new Attack(b1a_url, 50);
+        Defence b1d = new Defence(b1d_url, 50);
+        BehaviorLogic b1 = new BehaviorLogic(b1a, null);
+        BehaviorLogic b2 = new BehaviorLogic(null, b1d);
 
-        BehaviorLogic[] bls = {b8};
+        BehaviorLogic[] bls = {b1,b2,b1};
 
         String monster_url = ".\\images\\monster\\red_thief.png";
         int maxHp = 56;
